412d575e6456b7a014077c6566db5b53a4481ace
vundla <vundlamandlenkosi0@gmail.com>
2023-07-20 01:27:26 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*main - Test function fot positive or negative
+*Return: 0
+*/
+int main(void)
+{
+       int i;
+
+       i = 0;
+       positive_or_negative(i);
+
+       return (0);
+}
